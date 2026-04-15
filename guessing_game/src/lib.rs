c4f2cfb3b8473a275602787408d1c235f690612f
bellbird <45720449+C-amabilis@users.noreply.github.com>
2026-04-15 13:00:07 -0400

    generate a random number

--- guessing_game/src/lib.rs
+++ guessing_game/src/lib.rs
@@ -1,9 +1,13 @@
 use std::io;
 
+use rand::Rng;
+
 
 
 pub fn start() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("What is your guess?");
 
@@ -13,13 +17,6 @@ pub fn start() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    println!("The number was {secret_number}");
     println!("Your guess was {guess}");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {}
 }
